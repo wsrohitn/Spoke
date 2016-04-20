80de690be3a536f4c2098d8a7c81a8e48d63c5a5
Rohit Natarajan <rohit.natarajan@whitespace.co.uk>
2016-04-20 12:35:42 +0100

    Initial Commit

---
+++ Spoke/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spoke
+//
+//  Created by Rohit Natarajan on 20/04/2016.
+//  Copyright © 2016 Rohit Natarajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
